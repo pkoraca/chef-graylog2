39bbd45b856efbcde7d010da742a0a63f9bd9a0d
Petar Koraca <Petar.Koraca@infobip.com>
2014-07-03 22:00:03 +0200

    0.20.4 was released

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'graylog2'
 maintainer       'Petar Koraca'
-maintainer_email 'petar.koraca@gmail.com'
+maintainer_email 'pkoraca@gmail.com'
 license          'Apache'
 description      'Installs/Configures graylog2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports "centos"
 
