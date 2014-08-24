9e9527309480801154c5737f50309b99acaae3ad
Petar Koraca <Petar.Koraca@infobip.com>
2014-08-24 20:56:12 +0200

    fix: server logging; added is_master flag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pkoraca@gmail.com'
 license          'Apache'
 description      'Installs/Configures graylog2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 supports "centos"
 
