e694a80d36c6e5699d90a8ad105e024e76e6167b
Petar Koraca <Petar.Koraca@infobip.com>
2014-11-19 19:49:12 +0100

    Mongo version locked, graylog version bump

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'pkoraca@gmail.com'
 license          'Apache'
 description      'Installs/Configures graylog2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 supports "centos"
 
 depends "java"
-depends "mongodb"
+depends "mongodb", "0.16.1"
 depends "elasticsearch"
