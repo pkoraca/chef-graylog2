a97b253ddf5799fb3b9197e45c5099e75cde317f
Petar Koraca <Petar.Koraca@infobip.com>
2014-12-19 16:57:15 +0100

    graylog2::elasticsearch include Java recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pkoraca@gmail.com'
 license          'Apache'
 description      'Installs/Configures graylog2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.2.2'
 
 supports "centos"
 
